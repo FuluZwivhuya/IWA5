ba615666026e3d95da2d161dd6f29571e0543788
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-01-30 22:13:14 +0200

    not done

---
+++ scripts.js
@@ -0,0 +1,45 @@
+const FREE_WARNING = "Free shipping only applies to single customer orders";
+//const BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
+//const NONE_SELECTED = 0;
+
+var customers = 1 + '' +'customer';
+var currency = 'R';
+var shipping = 400;
+userRsa = 'South Africa';
+userNam = "Nambia";
+userOther = "Other";
+
+if(userRsa){console.log (shipping === 400 && currency === "R")};
+
+var currency ='$';
+var shipping = 600;
+if (userNam){console.log (shipping === 600 && currency === '$')};
+
+var shipping = 800
+if(userOther){console.log(shipping === 800 && currency === '$')};
+
+let shoes = 300 * 1;
+let toys = 100 * 5;
+let shirts = 150 * "NONE_SELECTED";
+let batteries = 35 * 2;
+let pens = 5 * "NONE_SELECTED";
+
+var shipping = null;
+zar = 1000;
+$ = 60
+
+console.log(zar,$);
+if (shoes + batteries + pens + shirts >= zar ||$) {console.log ("free shipping")} else {console.log("pay for shipment")};
+
+userNam = 
+/* if (location === RSA) shipping = 0 || calcShipping;
+  }
+}
+
+if (shipping === 0 && customers !== 1) {
+  console.log(FREE_WARNING);
+}
+
+if(location === "NK"){console.log(BANNED_WARNING)} else {console.log("price",currency,shoes + batteries + pens + shirts + shipping)};*/
+
+
