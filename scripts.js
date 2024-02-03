61d7a09ef4d81d84d76cc8be92a445980dc9f51d
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-03 23:19:47 +0200

    free warning

--- scripts.js
+++ scripts.js
@@ -1,41 +1,47 @@
-const FREE_WARNING = 'Free shipping only applies to single customer orders';
-const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const FREE_WARNING = "Free shipping only applies to single customer orders";
+const BANNED_WARNING ="Unfortunately we do not ship to your country of residence";
 const NONE_SELECTED = 0;
 
-let RSA;
+let customers = 1;
 let shipping;
+let currency;
+let locationA = "RSA";
+console.log(shipping, currency, locationA);
 
-shipping ='rsa'; if (shipping = 'rsa') {shipping === 400 && currency === 'R'}
-console.log ('shipping');
+if ((shipping = locationA)) {
+  shipping === 400 && currency === "R";
+}
+console.log("shipping");
 
-let NAM;
-shipping = 'nam'; if ( shipping = 'nam') {shipping === 600 && currency === '$'} else { shipping === 800 && currency === '$'}
-console.log('shipping');
+let locationB = "NAM";
+if ((locationB = "NAM")) {
+  shipping === 600 && currency === "$";
+} else {
+  shipping === 800 && currency === "$";
+}
+console.log("shipping");
 
 let shoes = 300 * 1;
 let toys = 100 * 5;
-let shirts = 150 * 'NONE_SELECTED'
-let batteries = 35 * 2
-let pens = 5 * 'NONE_SELECTED' 
-
-shipping = null
-let currency = '$';
-
- /*if (shoes + batteries + pens + shirts > 1000 && currency === '$'){
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	} 
-console.log (calcShipping);*/
+let shirts = 150 * 6;
+let batteries = 35 * 2;
+let pens = 5 * 0;
+console.log(shoes + toys); //test
+let calcShipping = Number
 
 
-//if (shipping = 0 && customers !== 1) 
-//console.log(FREE_WARNING);
+/*if (shoes + batteries + pens + shirts > 1000 && currency === '$'){ shipping = 0 || calcShipping};
+	if (locationB = 'NAM' && customers < 2) {shipping = 0 || calcShipping};
+			if (locationA = 'RSA')
+		    {shipping = 0 || calcShipping};*/
 
-let location = 'NK';
-if (location = 'NK'){console.log(BANNED_WARNING)} else {console.log('price', currency, shoes + batteries + pens + shirts)};
 
-let customers = 1
-//let location = 'RSA'
-currency = null
+if ((shipping = 0 && customers !== 1)) {
+  console.log(FREE_WARNING)};
+
+let locationC = "NK";
+if ((locationC === "NK")) 
+{console.warn(BANNED_WARNING)}
+
+currency = 'R';
+{console.log('price',':',currency,shoes + batteries + pens + shirts)};
