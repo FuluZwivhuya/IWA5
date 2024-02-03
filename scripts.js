a97d9a002566d85daf9fc7575915ac27818ea1f4
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-03 20:19:00 +0200

    yet to work on price

--- scripts.js
+++ scripts.js
@@ -1,45 +1,41 @@
-const FREE_WARNING = "Free shipping only applies to single customer orders";
-//const BANNED_WARNING = "Unfortunately we do not ship to your country of residence";
-//const NONE_SELECTED = 0;
+const FREE_WARNING = 'Free shipping only applies to single customer orders';
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence';
+const NONE_SELECTED = 0;
 
-var customers = 1 + '' +'customer';
-var currency = 'R';
-var shipping = 400;
-userRsa = 'South Africa';
-userNam = "Nambia";
-userOther = "Other";
+let RSA;
+let shipping;
 
-if(userRsa){console.log (shipping === 400 && currency === "R")};
+shipping ='rsa'; if (shipping = 'rsa') {shipping === 400 && currency === 'R'}
+console.log ('shipping');
 
-var currency ='$';
-var shipping = 600;
-if (userNam){console.log (shipping === 600 && currency === '$')};
-
-var shipping = 800
-if(userOther){console.log(shipping === 800 && currency === '$')};
+let NAM;
+shipping = 'nam'; if ( shipping = 'nam') {shipping === 600 && currency === '$'} else { shipping === 800 && currency === '$'}
+console.log('shipping');
 
 let shoes = 300 * 1;
 let toys = 100 * 5;
-let shirts = 150 * "NONE_SELECTED";
-let batteries = 35 * 2;
-let pens = 5 * "NONE_SELECTED";
+let shirts = 150 * 'NONE_SELECTED'
+let batteries = 35 * 2
+let pens = 5 * 'NONE_SELECTED' 
 
-var shipping = null;
-zar = 1000;
-$ = 60
+shipping = null
+let currency = '$';
 
-console.log(zar,$);
-if (shoes + batteries + pens + shirts >= zar ||$) {console.log ("free shipping")} else {console.log("pay for shipment")};
-
-userNam = 
-/* if (location === RSA) shipping = 0 || calcShipping;
-  }
-}
-
-if (shipping === 0 && customers !== 1) {
-  console.log(FREE_WARNING);
-}
-
-if(location === "NK"){console.log(BANNED_WARNING)} else {console.log("price",currency,shoes + batteries + pens + shirts + shipping)};*/
+ /*if (shoes + batteries + pens + shirts > 1000 && currency === '$'){
+	if (location = NAM && customers < 2) {
+			if (location = RSA)
+		    shipping = 0 || calcShipping
+		}
+	} 
+console.log (calcShipping);*/
 
 
+//if (shipping = 0 && customers !== 1) 
+//console.log(FREE_WARNING);
+
+let location = 'NK';
+if (location = 'NK'){console.log(BANNED_WARNING)} else {console.log('price', currency, shoes + batteries + pens + shirts)};
+
+let customers = 1
+//let location = 'RSA'
+currency = null
